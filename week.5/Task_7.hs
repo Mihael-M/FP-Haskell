7c30d9b987518a1680ea027b093b4650bb1eeed2
Mihael-M <158171091+Mihael-M@users.noreply.github.com>
2024-04-03 23:49:46 +0300

    task7

--- week.5/Task_7.hs
+++ week.5/Task_7.hs
@@ -1,13 +1,13 @@
-switchSum :: (Num a) => (a -> a) -> (a -> a) -> Int -> a -> a
-switchSum _ _ 0 = id
-switchSum f g n = (\x -> (f x) + switchSum g f (n - 1) x)
 
-
+import Data.List
+switchSum :: (Num a) => (a -> a) -> (a -> a) -> Int -> (a -> a) 
+switchSum _ _ 0 = (\x -> 0) 
+switchSum f g n = (\x -> f x + switchSum g f (n - 1) (f x))
 
 main :: IO()
 main = do
-    print $ (switchSum (\x -> x + 1) (\x -> x * 2) 1) 2 -- == 3
-    print $ (switchSum (\x -> x + 1) (\x -> x * 2) 2) 2 -- == 9
-    print $ (switchSum (\x -> x + 1) (\x -> x * 2) 3) 2 -- == 16
+    print $ (switchSum (\x -> x + 1) (\x -> x * 2) 1) 2 == 3
+    print $ (switchSum (\x -> x + 1) (\x -> x * 2) 2) 2 == 9
+    print $ (switchSum (\x -> x + 1) (\x -> x * 2) 3) 2 == 16
     print $ (switchSum (\x -> x + 1) (\x -> x * 2) 4) 2 == 30
     print $ (switchSum (\x -> x + 5) (\x -> x * 3) 4) 2 == 132 -- mytest
